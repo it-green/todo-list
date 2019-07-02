87a32c0de1048cec4e1e85beab964430a4c66a31
Ryotaro Kato <ryo.ctuad@gmail.com>
2019-07-02 14:19:30 +0900

    vuetify init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
-<style lang="stylus">
+<template lang="pug">
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+    v-app
+        v-toolbar-title.headline(text-uppercase)
+            span Vuetify
+        .button-area
+            v-btn(color="success") hello
+</template>
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Vuetify from 'vuetify';
+import HelloWorld from './components/HelloWorld.vue';
+
+Vue.use(Vuetify);
+
+@Component({
+    components: {
+        HelloWorld,
+    },
+})
+
+export default class App extends Vue {}
+</script>
+
+<style lang="stylus">
+@import '~vuetify/src/stylus/main'
 </style>
